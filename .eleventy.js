8a15fd1376f1f524e08e1eef4c72ed209b0803d3
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-08 11:55:42 -0800

    feat: hashing

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const criticalCSS = require("eleventy-critical-css");
+const cacheBuster = require("@mightyplow/eleventy-plugin-cache-buster");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight, {
@@ -8,6 +9,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(criticalCSS, {
         minify: true
     });
+    eleventyConfig.addPlugin(cacheBuster({}));
     eleventyConfig.addPassthroughCopy("js");
     return {
         dir: {
