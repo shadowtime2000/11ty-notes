95b56ebf200dfd471fc1bfddc5b0684e79865f58
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-08 15:09:59 -0800

    feat: bing webmaster support

--- src/_data/env.js
+++ src/_data/env.js
@@ -4,4 +4,5 @@ module.exports = {
     PAYMENT_POINTER: process.env.PAYMENT_POINTER,
     AUTHOR: process.env.AUTHOR,
     COPYRIGHT: process.env.COPYRIGHT,
+    BING_WEBMASTER: process.env.BING_WEBMASTER
 };
